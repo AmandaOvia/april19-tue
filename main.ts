df6e83cca632aa061e10c528d37e1d6699113df9
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-26 08:54:30 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,18 +3,17 @@
 input.onButtonPressed(Button.A, function () {
     for (let x = 0; x <= 4; x++) {
         for (let Y = 0; Y <= 0; Y++) {
-            led.plot(x, Y)
+            led.plot(Y, x)
             basic.pause(200)
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
-    for (let Y = 0; Y <= 4; Y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.toggle(x, Y)
-            basic.pause(200)
-        }
+    for (let x = 0; x <= 0; x++) {
+        led.plot(x, Y)
+        basic.pause(100)
     }
 })
-let x = 4
 let Y = 0
+let x = 4
+Y = 0
