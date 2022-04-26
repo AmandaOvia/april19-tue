0383ad32b0a8f576359018d50e1b684ffe137fca
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-26 09:19:14 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,15 @@
 // 0 1 21 3
 input.onButtonPressed(Button.A, function () {
     for (let x = 0; x <= 4; x++) {
-        for (let Y = 0; Y <= 0; Y++) {
-            led.plot(Y, x)
-            basic.pause(200)
-        }
+        let Y = 0
+        led.plot(Y, 4 - x)
+        basic.pause(200)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    for (let x = 0; x <= 0; x++) {
-        led.plot(x, Y)
-        basic.pause(100)
+    for (let Y = 0; Y <= 4; Y++) {
+        let x = 0
+        led.plot(Y, 4 - x)
+        basic.pause(200)
     }
 })
-let Y = 0
-let x = 4
-Y = 0
